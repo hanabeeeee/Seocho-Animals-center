575784948a9c43d872ad356f4482e455cf34ca44
hanabeeeee <105402245+hanabeeeee@users.noreply.github.com>
2022-08-29 11:56:30 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(function(){
     // ---------gnb-----------------
       // 1. 열기: #toggle-btn 클릭시 #gnb on
       $('#toggle-btn').click(function(){
-        $('#gnb').addClass('on');
+        $('#gnb').toggleClass('on');
       });
       // 2. 닫기: #btn-close 클릭시 #gnb 닫음
       $('#btn-close').click(function(){
